355d5df842e389042bc844c692ceef282a602ae8
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-01-20 07:03:45 +0000

    setting up basic structure in cli class

--- lib/metacritic/cli.rb
+++ lib/metacritic/cli.rb
@@ -2,6 +2,32 @@
 class Metacritic::CLI
 
   def call
-    puts "Welcome to Metacritic!"
+    menu
+    input = nil
+    while input != "exit"
+      puts "Movies or Games?"
+      input = gets.downcase.chomp
+      if input == "movies"
+        puts "movie_options"
+      elsif input == "games"
+        puts "games_options"
+      else
+        puts "Sorry, don't recognize your command."
+        input = gets.downcase.chomp
+      end
+    end
   end
+
+  def menu
+    puts "Welcome to Metacritic!"
+    puts "---------------------"
+    puts "This program will help you find movies or video games with good reviews!"
+    puts "NOTE: Type exit to leave program."
+    puts "+++++++++++++++++++++++++++++++++"
+  end
+
+  def movie_options
+
+  end
+
 end
