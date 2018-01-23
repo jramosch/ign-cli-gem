66aff06c3091e120d2fbbccc24a31a451e288610
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-01-23 21:14:57 +0000

    scraper class updated

--- lib/ign/scraper.rb
+++ lib/ign/scraper.rb
@@ -4,8 +4,8 @@ class IGN::Scraper
 
   @@scraped_games = []
 
-  def self.scrape_home
-
+  def self.scrape(ign_url)
+    html = ign_url
     doc = Nokogiri::HTML(open("http://www.ign.com/reviews/games"))
 
     games = doc.css("div.itemList-item")
@@ -13,7 +13,8 @@ class IGN::Scraper
       scraped_game = {}
       scraped_game[:name] = game.css("div.item-title a").text.strip
       scraped_game[:platform] = game.css("span.item-platform").text
-      scraped_game[:description] = game.css("")
+      scraped_game[:genre] = game.css("span.item-genre").text.strip
+      scraped_game[:rating] = game.css("span.scoreBox-score").text
     end
 
     game = self.new
