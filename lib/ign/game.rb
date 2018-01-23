daab2ef8ca7868c729905b13d256a76dade20b3b
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-01-23 19:09:24 +0000

    updated scraper class

--- lib/ign/game.rb
+++ lib/ign/game.rb
@@ -1,21 +1,12 @@
 class IGN::Game
   attr_accessor :name, :genre, :rating, :platform
 
-  def self.scrape_home
+  @@all = []
 
-    doc = Nokogiri::HTML(open("http://www.ign.com/reviews/games"))
-
-    game = self.new
-    game.name =
-
-    binding.pry
-
-  end
-
-  def self.scrape_editors_choice
-    doc = Nokogiri::HTML(open("http://www.ign.com/editors-choice/games"))
-
-
+  def initialize(game_hash)
+    student_hash.each {|key,value| self.send(("#{key}="),value)}
+    @@all << self
+    end
   end
 
 end
--- lib/ign/scraper.rb
+++ lib/ign/scraper.rb
@@ -1,4 +1,30 @@
 #this will collect game details into hashes to create game objects from
 
 class IGN::Scraper
+
+  @@scraped_games = []
+
+  def self.scrape_home
+
+    doc = Nokogiri::HTML(open("http://www.ign.com/reviews/games"))
+
+    games = doc.css("div.clear itemList-item")
+    games.each do |game|
+      scraped_game = []
+      scraped_game[:name] = game.css("")
+    end
+
+    game = self.new
+    game.name =
+
+    binding.pry
+
+  end
+
+  def self.scrape_editors_choice
+    doc = Nokogiri::HTML(open("http://www.ign.com/editors-choice/games"))
+
+
+  end
+
 end
