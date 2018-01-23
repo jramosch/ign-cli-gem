748c2b9f1c7791aaf14c7efe6cc953135793576d
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-01-23 23:30:36 +0000

    game and scraper classes updated

--- lib/ign/game.rb
+++ lib/ign/game.rb
@@ -8,5 +8,29 @@ class IGN::Game
     @@all << self
   end
 
+  def self.scrape(ign_url)
+    scraped_games = []
+    html = ign_url
+    doc = Nokogiri::HTML(open("http://www.ign.com/reviews/games"))
+
+    games = doc.css("div.itemList-item")
+
+    games.each do |game|
+      scraped_game = {}
+      scraped_game[:name] = game.css("div.item-title a").text.strip
+      scraped_game[:platform] = game.css("span.item-platform").text
+      scraped_game[:genre] = game.css("span.item-genre").text.strip
+      scraped_game[:rating] = game.css("span.scoreBox-score").text
+
+      scraped_games << scraped_game
+    end
+
+    scraped_games
+  end
+
+  def self.scrape_editors_choice
+    doc = Nokogiri::HTML(open("http://www.ign.com/editors-choice/games"))
+  end
+
 
 end
--- lib/ign/scraper.rb
+++ lib/ign/scraper.rb
@@ -2,32 +2,24 @@
 
 class IGN::Scraper
 
-  @@scraped_games = []
-
   def self.scrape(ign_url)
-    html = ign_url
-    doc = Nokogiri::HTML(open("http://www.ign.com/reviews/games"))
+    scraped_games = []
+    doc = Nokogiri::HTML(open(ign_url))
 
     games = doc.css("div.itemList-item")
+
     games.each do |game|
       scraped_game = {}
       scraped_game[:name] = game.css("div.item-title a").text.strip
       scraped_game[:platform] = game.css("span.item-platform").text
       scraped_game[:genre] = game.css("span.item-genre").text.strip
       scraped_game[:rating] = game.css("span.scoreBox-score").text
+
+      scraped_games << scraped_game
     end
 
-    game = self.new
-    game.name =
-
-    binding.pry
-
+    scraped_games
   end
 
-  def self.scrape_editors_choice
-    doc = Nokogiri::HTML(open("http://www.ign.com/editors-choice/games"))
-
-
-  end
 
 end
