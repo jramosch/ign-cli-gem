a49098bb62e709ed663b1bb352f2e90d14339c4a
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-02-08 05:47:03 +0000

    change hyphens to underscores for clarity

--- lib/ign-reviews.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'colorize'
-require 'pry'
-
-require_relative "./ign-reviews/version"
-require_relative "./ign-reviews/game"
-require_relative "./ign-reviews/cli"
-require_relative "./ign-reviews/scraper"
---
+++ lib/ign_reviews.rb
@@ -0,0 +1,9 @@
+require 'open-uri'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+
+require_relative "./ign_reviews/version"
+require_relative "./ign_reviews/game"
+require_relative "./ign_reviews/cli"
+require_relative "./ign_reviews/scraper"
